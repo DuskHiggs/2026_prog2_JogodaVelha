1690600e26933f26fa719c155ac3e91640431644
Emanuel <Emanuel@DESKTOP-9LG29U4>
2026-03-27 19:48:52 -0300

    monitor de eventos

---
+++ src/main/java/br/edu/bsi/prog2/monitoreventos/Monitoreventos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.bsi.prog2.monitoreventos;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class Monitoreventos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
